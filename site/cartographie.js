bc086426f736a20e762846d719b779fd653058c0
theo-oriol <theo.toriol.lol@gmail.com>
2020-04-06 17:17:23 +0200

    v1-site

---
+++ site/cartographie.js
@@ -0,0 +1,171 @@
+
+
+
+
+
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'orange',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: "white",
+        "fillOpacity": 0
+    };
+}
+
+
+
+function init(map){
+    
+    img = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGhlby1vcmlvbCIsImEiOiJjazhvYTQ1YWgwMGc1M21sbTVjM3BqeWM4In0.BB4gDDk54hPCKi_FfpOXtw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/light-v9',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(map);
+    region(map)
+}
+
+
+
+function goback(map,geojson){
+    geojson.clearLayers();
+    map.setView([46.83 ,3.642  ], 4.83);
+    region(map);
+}
+
+
+
+
+
+function region(map){
+    ///////////////////////////////////////////////////////////////
+    var info = L.control();
+    
+    info.onAdd = function (map) {
+		this._div = L.DomUtil.create('div', 'info');
+		this.update();
+		return this._div;
+	};
+        
+    info.update = function (props) {
+		this._div.innerHTML = (props ? '<b> Nom : ' + props.nom+'':'');
+	};
+    
+    info.addTo(map);
+    ///////////////////////////////////////////////////////////////
+    function highlightFeature(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            weight: 5,
+            color: 'white',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+        info.update(layer.feature.properties);
+    };
+
+    function resetHighlight(e) {
+            geojson.resetStyle(e.target);
+            info.update();
+    }
+    function zoomToFeature(e) {
+        map.fitBounds(e.target.getBounds());
+        console.log(e.target.feature.properties.nom)
+        geojson.clearLayers()
+        commune(map)
+    }
+    ///////////////////////////////////////////////////////////////
+    geojson = L.geoJson(dep, {
+		onEachFeature: onEachFeature,
+        style: style
+	}).addTo(map);
+    
+    function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+    ///////////////////////////////////////////////////////////////
+}
+function commune(map){
+        fetch('cantons-34-herault.geojson')
+              .then((response) => {
+                return response.json();
+              })
+              .then((data) => {
+            ///////////////////////////////////////////////////////////////
+            var info = L.control();
+
+            info.onAdd = function (map) {
+                this._div = L.DomUtil.create('div', 'info');
+                this.update();
+                return this._div;
+            };
+
+            info.update = function (props) {
+                this._div.innerHTML = (props ? '<b> Nom : ' + props.nom+'':'');
+            };
+
+            info.addTo(map);
+            ///////////////////////////////////////////////////////////////
+                
+            
+            function highlightFeature(e) {
+                    var layer = e.target;
+
+                    layer.setStyle({
+                        weight: 5,
+                        color: 'white',
+                        dashArray: '',
+                        fillOpacity: 0.7
+                    });
+
+                    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                        layer.bringToFront();
+                    }
+                    info.update(layer.feature.properties);
+                };
+
+                function resetHighlight(e) {
+                        geojson.resetStyle(e.target);
+                        info.update();
+                }
+                function zoomToFeature(e) {
+                    map.fitBounds(e.target.getBounds());
+                    console.log(e.target.feature.properties.nom)
+                }
+                ///////////////////////////////////////////////////////////////
+                
+
+                geojson = L.geoJson(data, {
+                    onEachFeature: onEachFeature,
+                    style: style
+                }).addTo(map);
+
+                function onEachFeature(feature, layer) {
+                    layer.on({
+                        mouseover: highlightFeature,
+                        mouseout: resetHighlight,
+                        click: zoomToFeature
+                    });
+                }
+
+
+            });
+         };
+var map = L.map('map').setView([46.83 ,3.642  ], 4.83);
+var geojson;
+init(map);
