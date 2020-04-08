c0c83718c227f257c4f018e4d1acbddb02915d48
SylvainPovia <56920602+SylvainPovia@users.noreply.github.com>
2020-04-08 12:08:24 +0200

    Add Icone

--- index.js
+++ index.js
@@ -10,12 +10,31 @@
 		node.setAttribute("id","diagno")         // Create a text node
                               // Append the text to <li>
 document.getElementById("main1").appendChild(node);   
-
+icone = ["./icone/biberon.svg","./icone/habitants.svg","./icone/habitants2.svg",
+"./icone/hlm.svg","./icone/Médecin.svg","./icone/pollution.svg","./icone/recherche_emploi.svg",
+"./icone/revenus.svg","./icone/under18.svg"]
 indic = "<br><br>"
 for (var i = 0; i < 9; i++) {
-	next_indic = "<div class='col-sm-4 ' style='margin-bottom:15px;margin-top:15px;'><div class='card shadow' style='background-color: #D8D8D8;'>"+
-	"<div class='card-body rounded border-secondary' >"+
-"<span class='card-text'>Indicateur "+ i +"</p></div></div></div>"
+next_indic = "<div class='col-sm-4 ' style='margin-bottom:15px;margin-top:15px;'>"+
+                     "<div class='card-img-top d-flex align-items-center border-secondary rounded shadow-sm' style='background-color: #D8D8D8' '>"+
+            "<div>"+
+                "<img class='img-fluid' src="+ icone[i]+ " style='width: 50px;height: 50px;padding :10px' alt='Card image cap'>"+
+            "</div>"+
+            "<p class='col p-2 m-0' text-center> Indicateur "+ i + "</p>"+
+       " </div>"+
+                        "</div>"
+
+      /*  next_indic =      "<div class='card col-sm-4 w-25' style='margin:15px'>"+
+        "<div class='card-img-top d-flex align-items-center bg-light style='background-color: #D8D8D8' '>"+
+            "<div>"+
+                "<img class='img-fluid' src="+ icone[i]+ " style='width: 50px;height: 50px;padding :10px' alt='Card image cap'>"+
+            "</div>"+
+            "<p class='col p-2 m-0'>Indicateur "+ i + "</p>"+
+       " </div>"+
+ " </div>" */
+
+
+
 	indic = indic + next_indic
 }
 
@@ -41,43 +60,38 @@ document.getElementById("main1").appendChild(node2);
 
 pred = ""
 for (var i = 0; i < 3; i++) {
-	/*next_pred = "<div class='card border-warning mb-4' style='max-width: 18rem;margin: 15px''>"+
+  /*next_pred = "<div class='card border-warning mb-4' style='max-width: 18rem;margin: 15px''>"+
   "<div class='card-body'>"+
     "<p class='card-text'> Indicateur "+ i +" !  | Attention dans quelques jours les problemes vont arriver ! </p>"+
   "</div></div>" */
 
 
-  next_pred =  "<div class='row'><div class='card col-md-' style='margin-bottom:15px'><div class='card-body'>"+
-        "This is some text within a card body.</div></div>"+
-        "<span class='text-center col-6 ' style='padding-top: 15px;'> Attention ceci est une mise en garde ! Problems are coming ! </span></div><div></div>"
+  next_pred =  "<div class='row'>"+
+                    "<div class='card col-md-' style='margin-bottom:15px'>" +
+                          "<div class='card-body'>"+
+                                "This is some text within a card body.</div></div>"+
+                                "<span class='text-center col-6 ' style='padding-top: 15px;'> "+
+                                  "Attention ceci est une mise en garde ! Problems are coming !"+
+                                "</span>"+
+                          "</div>"+
+                      "<div>"+
+                "</div>"
 
-	pred = pred + next_pred
+  pred = pred + next_pred
 }
-console.log(pred)
-//https://giphy.com/gifs/BM0NZ2IUusz8NsAIFj/html5
+
 document.getElementById("main2").innerHTML = pred
  top = document.getElementById("main2");
-top.innerHTML += "<br><div class='text-center'><br>"+
+
+  top.innerHTML += "<br><div class='text-center'><br>"+
     	"<h1> ALLEZ PLUS LOIN AVEC CARTOSANTE </h1><br>"+
     	"<a href='https://sirse.atlasante.fr/#c=indicator' target='_blank'>"+
     	"<img src='https://media.giphy.com/media/BM0NZ2IUusz8NsAIFj/giphy.gif' /></a></div>"
 
-//top.innerHTML = "<div class='d-flex justify-content-between'>"+indic+"</div>"
+
+
 	}
-                                // Append the text to <li>
-//document.getElementById("id1").appendChild(node);     // Append <li> to <ul> with id="myList
 
-//document.getElementById("id1").innerHTML= "<div class='card'><div class='card-body'>This is some text within a card body.</div></div>"
 
-top.innerHTML += "<div class='card flex-row flex-wrap'>"+
-        "<div class='card-header border-0'>"+
-            "<img src='https://media.giphy.com/media/BM0NZ2IUusz8NsAIFj/giphy.gif'>"+
-        "</div>"+
-        "<div class='card-block px-2'>"+
-            "<h4 class='card-title'>Title</h4>"+
-            "<p class='card-text'>Description</p>"+
-            "<a href='#' class='btn btn-primary'>BUTTON</a>"+
-        "</div><div class='w-100'></div>"+
-        "<div class='card-footer w-100 text-muted'>FOOTER</div></div>"
 
 
