47f53c861a7053b9bcb6a8c2758e30caa01eb98c
lamouche <clement.cisterne@outlook.fr>
2020-04-08 13:30:35 +0200

    tri dans l'arborescence

--- js/indicators.js
+++ js/indicators.js
@@ -23,7 +23,7 @@ ind_names =
 	['NSP19'		, 'Nombre de sites et sols pollués (ou potentiellement pollués) 2019'],
 	['DP16'			, 'Densité de Population 2016'],
 	['PGP19'		, 'Part de médecins généraliste libéral au 31/12/2019'], 
-	['PGP19_60>'  	, 'Part de médecins généraliste libéral de plus de 60 ans au 31/12/2019'],
+	['PGP19_>60'  	, 'Part de médecins généraliste libéral de plus de 60 ans au 31/12/2019'],
 	['PC16-15_24'	, 'Part de chômeurs chez les 15-24 ans (RP) 2016'],
 	['PC16-15_64'	, 'Part de chômeurs chez les 24-64 ans (RP) 2016'],
 	['PPop16_<18' 	, 'Part de population des moins de 18 ans 2016'], 
