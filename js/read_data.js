41f45e4f95a7ac27b303ea8f61503bfc9efe19a3
lamouche <clement.cisterne@outlook.fr>
2020-04-09 15:47:22 +0200

    lecture des 2 json différents

--- js/read_data.js
+++ js/read_data.js
@@ -30,9 +30,28 @@ function loadData() {
           // on lance la fonction de callback avec le catalogue récupéré
           //callback();
         }
-      }
+      } 
       xhr.open("GET", "Stats/data.json", true);
       xhr.send();
       return catalogue
 }
+
+catalogue2 = []
+function loadData2() {
+      var xhr = getXMLHttpRequest();
+      // on réagit à l'événement onreadystatechange
+      xhr.onreadystatechange = function() {
+        // test du statut de retour de la requête AJAX
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+          // on désérialise le catalogue et on le sauvegarde dans une variable
+          catalogue2 = JSON.parse(xhr.responseText.replace(/\bNaN\b/g, null));
+          // on lance la fonction de callback avec le catalogue récupéré
+          //callback();
+        }
+      } 
+      xhr.open("GET", "Stats/data_display.json", true);
+      xhr.send();
+      return catalogue2
+}
+
 
